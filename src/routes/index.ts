c55f9dba65c6c382c6e938c3fb5d3b31776d2c1d
Tiago Ribeiro <tiagoribeiro.1803@gmail.com>
2024-08-08 18:23:42 +0100

    foo

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,12 @@
 import Router from "@koa/router";
 
+const { version } = require("../../package.json");
+
 const router = new Router();
 
-router.get("/api/v1/health", (ctx) => (ctx.body = "OK"));
+router
+  .get("/api/v1/health", (ctx) => (ctx.body = "OK"))
 
-router.get("/api/v1/version", (ctx) => (ctx.body = { minVersion: 1, version: 1 }));
+  .get("/api/v1/version", (ctx) => (ctx.body = { minVersion: 1, version: Number(version) }));
 
 export default router;
