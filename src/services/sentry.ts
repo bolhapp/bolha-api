ff7661573919f2c94b3fdbc4c9ca6c0f23184844
Tiago Ribeiro <tiagoribeiro.1803@gmail.com>
2024-09-11 18:06:38 +0100

    fix: only have sentry in prod

--- src/services/sentry.ts
+++ src/services/sentry.ts
@@ -5,20 +5,18 @@ import { version } from "../../package.json";
 
 const env = process.env.NODE_ENV;
 
-// Ensure to call this before importing any other modules!
-Sentry.init({
-  dsn: "https://f1e877cef9573b474a55a4e73889f39d@o4507935073107968.ingest.de.sentry.io/4507935077498960",
-  environment: env,
-  release: `${env}_${version}`,
-  integrations: [
-    // Add our Profiling integration
-    nodeProfilingIntegration(),
-  ],
+if (env !== "development") {
+  Sentry.init({
+    dsn: "https://f1e877cef9573b474a55a4e73889f39d@o4507935073107968.ingest.de.sentry.io/4507935077498960",
+    environment: env,
+    release: `${env}_${version}`,
+    integrations: [nodeProfilingIntegration()],
 
-  ...(env === "development"
-    ? { tracesSampleRate: 1.0, profilesSampleRate: 1.0, debug: true }
-    : { tracesSampleRate: 0.1, profilesSampleRate: 0.1, debug: false }),
-});
+    ...(env === "development"
+      ? { tracesSampleRate: 1.0, profilesSampleRate: 1.0, debug: true }
+      : { tracesSampleRate: 0.1, profilesSampleRate: 0.1, debug: false }),
+  });
+}
 
 export const logError = Sentry.captureException;
 
